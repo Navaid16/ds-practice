a28d88feb374db28c6ed100faf63fb544d9bacbc
Navaid16 <navaidakhtar16@gmail.com>
2026-09-22 10:29:03 +0530

    message

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int factN(int n) {
+    if(n == 1)
+        return 1;
+
+    return n * factN(n - 1);
+
+}
+
+
+int main() {
+    int n = 5;
+
+    cout << factN(n);
+
+}
